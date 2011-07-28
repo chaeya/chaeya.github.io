457c1e34be80d1ac15037f6c3cd27ffbb7d92404
Hyeongchae Kim <chaeya@gmail.com>
2011-07-28 09:31:41 +0900

    draft version

---
+++ js/script.js
@@ -0,0 +1,30 @@
+// Thanks janl and nslater
+// hosted scripts
+
+scripts = [
+    "http://www.google-analytics.com/ga.js",
+]
+
+// load hosted scripts
+for(i = 0; i < scripts.length; i++) {
+    document.write("<script src='" + scripts[i] + "'></script>");
+}
+
+
+// run after hosted scripts have loaded
+$(document).ready(function() {
+    // catch all errors
+    $('include').include();
+    try {
+        // wrap all elements with an id with an anchor
+        $("*/[id!='']").wrap(function() {
+            return "<a class='anchor' href='#" + $(this).attr("id") + "' />";
+        });
+
+        // activate analytics tracking
+        _gat._getTracker("UA-XXXXX-Y")._trackPageview();
+    } catch (error) {
+        // uh oh, better have a debug console handy
+        consol.log(error);
+    }
+});
